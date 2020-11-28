780a93d03c1e0a38633bbbb398507a8d7df4514a
Daniel Narvaez <narvd997@newschool.edu>
2020-11-28 15:04:37 -0500

    initial commit

---
+++ Assets/Scripts/LookAround.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class LookAround : MonoBehaviour
+{
+    PlayerControls controls;
+    Vector2 move;
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+    float xRotation = 0f;
+
+    void Awake()
+    {
+        controls = new PlayerControls();
+        controls.LookAround.Rotation.performed += ctx => move = ctx.ReadValue<Vector2>();
+        controls.LookAround.Rotation.canceled += ctx => move = Vector2.zero;    
+    }
+
+    void OnEnable()
+    {
+        controls.LookAround.Enable();
+    }
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        playerBody = transform.parent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float mouseX = move.x * mouseSensitivity * Time.deltaTime;
+        float mouseY = move.y * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 45f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    public CharacterController _characterController;
+    PlayerControls controls;
+    Vector2 move;
+    public float speed = 12f;
+
+    //Gravity
+    public float gravity = -9.81f;
+    Vector3 velocity;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.4f;
+    public LayerMask groundLayer;
+    public bool grounded;
+
+    void Awake()
+    {
+        controls = new PlayerControls();
+
+        controls.WalkAround.Movement.performed += ctx => move = ctx.ReadValue<Vector2>();
+        controls.WalkAround.Movement.canceled += ctx => move = Vector2.zero;
+    }
+
+    void OnEnable()
+    {
+        controls.WalkAround.Enable();
+    }
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+        groundCheck = transform.Find("GroundCheck");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        grounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer);
+        if(grounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        
+        float x = move.x;
+        float z = move.y;
+
+        Vector3 movement = transform.right * x + transform.forward * z;
+        _characterController.Move(movement * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        _characterController.Move(velocity * Time.deltaTime);
+    }
+}
